f6f17adef73a97789136e11bd5ab92b017effa62
jaisnan <jaisnan@amazon.com>
2023-07-13 17:57:52 +0000

    add script and tests

--- tests/coverage/reach/wrong_coverage_2/main.rs
+++
@@ -1,8 +0,0 @@
-// Copyright Kani Contributors
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-#[kani::proof]
-fn wrong_coverage_2() {
-    let a: u8 = kani::any();
-    kani::assume(false);
-    assert!(a < 5);
-}
